0ea6a289c0b361d127ff8826f6a155e557e4d317
Victor Hugo <victormotabatista@gmail.com>
2022-10-31 13:10:06 -0300

    feat(migrations): update migration

---
+++ Migrations/20221031160839_tarefaControllersUpdate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrilhaApiDesafio.Migrations
+{
+    public partial class tarefaControllersUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
